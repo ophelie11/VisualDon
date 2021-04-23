087ac1e5ec23a797253951b7d12c5e2ffaa12793
ophelie.baur <ophelie.baur@heig-vd.ch>
2021-04-23 12:02:55 +0200

    exercice1

---
+++ 20210423/carte-d3/preparer/arbres.js
@@ -0,0 +1,11 @@
+const data = require('./map.json')
+
+const result = data.features
+  .filter(d => d.geometry.type === 'Point' && d.properties.natural === 'tree')
+  .map(d => d.geometry.coordinates)
+
+console.log(
+  JSON.stringify(
+    result
+  )
+)
---
+++ 20210423/carte-d3/preparer/run.sh
@@ -0,0 +1,11 @@
+# convertir en json
+osmtogeojson map.osm > map.json
+
+# extraires les arbres
+node arbres > ../src/arbres.json
+
+# extraires les bâtiments
+node batiments > ../src/batiments.json
+
+# extraires les routes
+node routes > ../src/routes.json
